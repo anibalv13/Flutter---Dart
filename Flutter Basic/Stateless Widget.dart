e6b09d2032a1559ec4a0d6c189a9f56ba72b220d
Anibalv <asenciovd@gmail.com>
2022-06-29 09:33:57 -0400

    Flutter & Dart

---
+++ Flutter Basic/Stateless Widget.dart
@@ -0,0 +1,15 @@
+class Frog extends StatelessWidget {
+    const Frog({
+        Key key,
+        this.color,
+        this.child,
+}): super(key:key);
+
+    final Color color;
+    final widget child;
+
+    @override
+    Widget build(BuildContext context){
+        return Container(color: color, child: child);
+    }
+}
