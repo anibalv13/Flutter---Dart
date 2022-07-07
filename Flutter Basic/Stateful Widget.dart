77645c3f1c0d7eb527c749c592b8b5c48b3d8f4a
Anibalv <asenciovd@gmail.com>
2022-07-07 12:48:24 -0400

    Coceptos

--- Flutter Basic/Stateful Widget.dart
+++ Flutter Basic/Stateful Widget.dart
@@ -1,3 +1,12 @@
+/**
+ * Es un widget con estado, permite dibujar en pantalla,
+ * pero este, a su vez , tiene métodos para poder cambiar los valores,
+ * es decir, poder mostrar los cambios en  pantalla, para ello
+ * basta con llamar al método "setState", cambiar los valores que se imprimen, 
+ * y el cambio es inmediato, debido a que se vuelve a dibujar toda la interfaz necesaria.
+ */
+
+//conta de la clase del statefull 
 /* class Bird extends StatefulWidget{
   const Bird({
     Key key,
@@ -7,15 +16,17 @@
 
   final Color color;
   final Widget child;
-
+//aquí se genera el cambio de estado
   _BirdState createState( )=> _BirdState();
 }
+//clase de estado de la clase padre sin contructor 
 class _BirdState extends State<Bird> {
   double _size =1.0;
   void grow(){
     setState((){ _size += 0.1;});
   }
   @override
+  //aquí si se puede cambiar la posicion de un container dentro del build
   Widget Build(BuildContext context){
     return Container(
       color: widget.color,
--- Flutter Basic/Stateless Widget.dart
+++ Flutter Basic/Stateless Widget.dart
@@ -1,4 +1,11 @@
+/**son widget sin estado, que nos permiten dibujar algo una sola vez, 
+ * sin cambiar los valores mostrados, 
+ * se usa generalmente para mostar partes de la interfaz que
+ * no deben cambiar sus valores o ellos mismo deben controlar el cambio
+ * */ 
+
 /* class Frog extends StatelessWidget {
+  //constructor de tipo const, buena practica
     const Frog({
         Key key,
         this.color,
@@ -7,9 +14,10 @@
 
     final Color color;
     final widget child;
-
+//metodo build  retornar un arbol de widgets a dibujar o instanciar
+//renderiza 
     @override
     Widget build(BuildContext context){
         return Container(color: color, child: child);
     }
-} */
+}  */
